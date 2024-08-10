a40127aa2f18772ab86403345a16b228b275473a
atomicswiftdev <atomicswiftdev@gmail.com>
2024-08-10 20:22:14 +0100

    Remove unneeded template code

--- AbsolutelyBrickingIt/GameViewController.swift
+++ AbsolutelyBrickingIt/GameViewController.swift
@@ -15,14 +15,9 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: CGSize(width: 375.0, height: 667.0))
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
