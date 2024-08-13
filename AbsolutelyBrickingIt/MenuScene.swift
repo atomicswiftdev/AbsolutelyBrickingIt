7bf9249668041ba8070971c58c2f16919299fcbb
atomicswiftdev <atomicswiftdev@gmail.com>
2024-08-13 18:20:43 +0100

    Add images as nodes in the menu scene

--- AbsolutelyBrickingIt/MenuScene.swift
+++ AbsolutelyBrickingIt/MenuScene.swift
@@ -10,6 +10,20 @@ import SpriteKit
 class MenuScene: SKScene {
     
     override func didMove(to view: SKView) {
+        let backgroundNode = SKSpriteNode(imageNamed: "background.menu")
+        backgroundNode.position = CGPoint(x: 0.5 * size.width, y: 0.5 * size.height)
+        addChild(backgroundNode)
         
+        let logoNode = SKSpriteNode(imageNamed: "logo")
+        logoNode.position = CGPoint(x: 0.5 * size.width, y: 0.8 * size.height)
+        addChild(logoNode)
+        
+        let aboutNode = SKSpriteNode(imageNamed: "button.about")
+        aboutNode.position = CGPoint(x: 0.5 * size.width, y: 0.32 * size.height)
+        addChild(aboutNode)
+        
+        let playNode = SKSpriteNode(imageNamed: "button.play")
+        playNode.position = CGPoint(x: 0.5 * size.width, y: 0.2 * size.height)
+        addChild(playNode)
     }
 }
