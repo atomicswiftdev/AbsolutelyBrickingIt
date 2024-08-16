11f6d43cdda94ee0f6ae0c0eade29e087521736f
atomicswiftdev <atomicswiftdev@gmail.com>
2024-08-16 21:03:41 +0100

    Add background and paddle to game scene

--- AbsolutelyBrickingIt/GameScene.swift
+++ AbsolutelyBrickingIt/GameScene.swift
@@ -16,7 +16,13 @@ class GameScene: SKScene {
     weak var coordinator: GameSceneCoordinator?
     
     override func didMove(to view: SKView) {
+        let backgroundNode = SKSpriteNode(imageNamed: "background.menu")
+        backgroundNode.position = CGPoint(x: 0.5 * size.width, y: 0.5 * size.height)
+        addChild(backgroundNode)
         
+        let paddleNode = SKSpriteNode(imageNamed: "paddle")
+        paddleNode.position = CGPoint(x: 0.5 * size.width, y: 0.1 * size.height)
+        addChild(paddleNode)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
