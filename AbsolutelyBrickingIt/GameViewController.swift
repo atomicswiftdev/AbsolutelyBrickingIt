d8d7e77dafed0756ef4a2f20dd5d676a3f1c461b
atomicswiftdev <atomicswiftdev@gmail.com>
2024-08-21 22:05:34 +0100

    Add GameOverScene variable and coordinator implementation to GameViewController

--- AbsolutelyBrickingIt/GameViewController.swift
+++ AbsolutelyBrickingIt/GameViewController.swift
@@ -73,6 +73,13 @@ private extension GameViewController {
         scene.scaleMode = .resizeFill
         return scene
     }
+    
+    var gameOverScene: GameOverScene {
+        let scene = GameOverScene(size: sceneSize)
+        scene.coordinator = self
+        scene.scaleMode = .resizeFill
+        return scene
+    }
 }
 
 // MARK: - Menu scene coordinator
@@ -103,5 +110,18 @@ extension GameViewController: GameSceneCoordinator {
     
     func gameSceneGameEnded(_ scene: GameScene) {
         print("Show game over scene")
+    }
+}
+
+// MARK: - Game Over scene coordinator
+
+extension GameViewController: GameOverSceneCoordinator {
+    
+    func gameOverSceneMenuTapped(_ scene: GameOverScene) {
+        present(scene: menuScene)
+    }
+    
+    func gameOverScenePlayTapped(_ scene: GameOverScene) {
+        present(scene: gameScene)
     }
 }
